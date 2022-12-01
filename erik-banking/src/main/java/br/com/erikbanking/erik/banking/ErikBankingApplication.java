4701c160341daae7b4b702f48d6d8e8289c0b364
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-12-01 20:01:35 -0300

    segundo curso

---
+++ erik-banking/src/main/java/br/com/erikbanking/erik/banking/ErikBankingApplication.java
@@ -0,0 +1,25 @@
+package br.com.erikbanking.erik.banking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@EnableAutoConfiguration
+@ComponentScan
+@SpringBootApplication
+public class ErikBankingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ErikBankingApplication.class, args);
+	}
+
+	@GetMapping("/teste")
+		public String index (){
+			return "Ola Mundo ";
+		}
+
+}
---
+++ erik-banking/src/test/java/br/com/erikbanking/erik/banking/ErikBankingApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.erikbanking.erik.banking;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ErikBankingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
