2c9dec9f3c2a9019260fe23775103d69b713f0a3
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2022-12-04 18:21:11 -0300

    post de pacientes

--- api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ api/src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -3,7 +3,7 @@ package med.voll.api.medico;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
-        String nome, String email,
+        String nome, String telefone, String email,
         String crm, Especialidade especialidade,
         DadosEndereco endereco) {
 
---
+++ api/src/main/java/med/voll/api/paciente/DadosCadastrarPaciente.java
@@ -0,0 +1,9 @@
+package med.voll.api.paciente;
+
+import med.voll.api.endereco.DadosEndereco;
+
+public record DadosCadastrarPaciente (
+               String nome, String email,
+               String telefone,String cpf,
+               DadosEndereco endereco) {
+}
