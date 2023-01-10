2821119bb505ab42c66c22d4f7d08495ad79cb76
Bruno Rossi Serra <117949816+BrunoRossiSerra@users.noreply.github.com>
2023-01-10 19:14:21 -0300

    metodo get v1

--- api/src/main/java/med/voll/api/controller/MedicoController.java
+++ api/src/main/java/med/voll/api/controller/MedicoController.java
@@ -3,14 +3,14 @@ package med.voll.api.controller;
 
 import jakarta.validation.Valid;
 import med.voll.api.medico.DadosCadastroMedico;
+import med.voll.api.medico.DadosListagemMedicos;
 import med.voll.api.medico.Medico;
 import med.voll.api.medico.MedicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/medicos")
@@ -24,4 +24,9 @@ public class MedicoController {
         medicoRepository.save(new Medico(dados));
     }
 
+    @GetMapping
+    public List<DadosListagemMedicos> lista(){
+        return medicoRepository.findAll().stream().map(DadosListagemMedicos::new).toList();
+
+    }
 }
